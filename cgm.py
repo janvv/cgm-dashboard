c50b6dfea8fa98b3daabb4386faf26dcb8bf1015
jan <wrede.jan@gmail.com>
2019-11-01 16:29:39 +0100

    Bugfix: Not loading new entries
    The abckend takes a while until new entries are available. However, cgmAccess logic  only queried each time frame once. If nothing was returned, we would not query a second time.
    This bugfix only updates "latest_query_time" if new values were found and sets it to the latest timestamp of the retuned values

--- cgm.py
+++ cgm.py
@@ -39,37 +39,41 @@ class CGMAccess:
         now = datetime.now()
         t_end = now.timestamp()-skip_hours*60*60
         t_start = 0 if limit_days == 0 else (now - timedelta(days=limit_days)).timestamp()
+        datetime_latest_queried_item = self.latest_query_time
 
-        #print("LOOKING FOR : {} - {} ...".format(t_start, t_end))
-        #print("EXISTING    : {} - {}".format(self.earlierst_query_time, self.latest_query_time))
+        print("LOOKING FOR : {} - {} ...".format(datetime.fromtimestamp(t_start), datetime.fromtimestamp(t_end)))
+        print("EXISTING    : {} - {}".format(datetime.fromtimestamp(self.earlierst_query_time),
+                                             datetime.fromtimestamp(self.latest_query_time)))
 
         if (t_start > self.earlierst_query_time) and (t_start < self.latest_query_time):
             t_start = self.latest_query_time
 
-        if t_end-t_start < 10:
-            print("IGNORE because time since last query < 10 seconds")
+        try:
+
+            #query missing data
+            entries = self.db['entries']
+            print("QUERYING    : {} - {}".format(datetime.fromtimestamp(t_start), datetime.fromtimestamp(t_end)))
+            results = entries.find({"sgv": {"$gt": 0}, "date": {"$gt": t_start*1000, "$lt": t_end*1000}},
+                                   ["sgv", "date"], sort=[("date", DESCENDING)])
+            tuples = [(datetime.fromtimestamp(r["date"] / 1000), r["sgv"]) for r in results]
+
+            if len(tuples) > 0:
+                print("queried {} new entries".format(len(tuples)))
+                temp_df = pd.DataFrame(data=tuples, columns=[self.DATETIME_COLUMN, self.GLUCOSE_COLUMN])
+                self.df = self.df.append(temp_df, sort=False, ignore_index=True).drop_duplicates()
+                #TODO: ATM  we can not use the last item from the pandas dataframe because it gives a different timestamp (tizezone specific)
+                t, g = zip(*tuples)
+                datetime_latest_queried_item = max(t).timestamp()
+                print("latest found = {}".format(max(t)))
+        except errors.PyMongoError as e:
+            self.logger.error("Error while querying for last entries: \n {}".format(e))
+
         else:
-            try:
-
-                #query missing data
-                entries = self.db['entries']
-                #print("QUERYING   : {} - {}".format(t_start, t_end))
-                results = entries.find({"sgv": {"$gt": 0}, "date": {"$gt": t_start*1000, "$lt": t_end*1000}},
-                                       ["sgv", "date"], sort=[("date", DESCENDING)])
-                tuples = [(datetime.fromtimestamp(r["date"] / 1000), r["sgv"]) for r in results]
-
-                if len(tuples) > 0:
-                    print("queried {} new entries".format(len(tuples)))
-                    temp_df = pd.DataFrame(data=tuples, columns=[self.DATETIME_COLUMN, self.GLUCOSE_COLUMN])
-                    self.df = self.df.append(temp_df, sort=False, ignore_index=True).drop_duplicates()
-
-            except errors.PyMongoError as e:
-                self.logger.error("Error while querying for last entries: \n {}".format(e))
-
-            else:
-                # update query times if successfull
-                self.earlierst_query_time = min(t_start, self.earlierst_query_time) if (self.earlierst_query_time != -1) else t_start
-                self.latest_query_time = t_end
+            # update query times if successfull
+            self.earlierst_query_time = min(t_start, self.earlierst_query_time) if (self.earlierst_query_time != -1) else t_start
+            #we can not be sure that db is fast enough to return values immediately, therefore end-time is only updated if we received a value
+            #if we would update to t_end right away, we would not find it if it wasnt returned immediately
+            self.latest_query_time = datetime_latest_queried_item
 
 
     def get_entries(self, n_days=14):
@@ -108,14 +112,12 @@ if __name__ == '__main__':
     print("all")
     access = CGMAccess()
 
+    access.update_entries(limit_days=14, skip_hours=10/60)
+    df = access.get_entries(14)
 
-    access.update_entries()
-    df = access.get_entries(90)
-    df.to_json()
-    print(len(df))
-
-    stats = agp.calculateHourlyStats(df,access.DATETIME_COLUMN,access.GLUCOSE_COLUMN)
-    print(stats)
-    agp.drawAGP(df, access.DATETIME_COLUMN, access.GLUCOSE_COLUMN)
-    plt.show()
+    while True:
+        access.update_entries(limit_days=14)
+        latest = access.get_last_entry()
+        print(latest["datetime"],latest["glucose"])
+        time.sleep(11)
 
