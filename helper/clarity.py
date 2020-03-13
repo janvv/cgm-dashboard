1b58b8931b98054fb30920d1501715b1b8a23a91
Jan Wrede <wrede.jan@gmail.com>
2020-03-13 21:14:03 +0100

    bugfixes *convert numbers failed when reading strings of floats *changed everything to english clarity files

--- helper/clarity.py
+++ helper/clarity.py
@@ -2,25 +2,48 @@ import pandas as pd
 from datetime import datetime, timedelta
 
 def convert(x, low_description='Niedrig'):
-    if str(x).isdigit():
-        return int(x)
-    elif x == low_description:
-        return 30
-    else:
-        return 400
+    try:
+        value = float(x)
+    except ValueError:
+        if x.lower() == "low":
+            value = 40
+        elif x.lower() == "high":
+            value = 400
+        else:
+            raise ValueError("can't decompile {}".format(x))
+    return float(value)
+    #elif x == low_description:
+    #    return 30
+    #else:
+    #    return 400
 
 def day_of_year(date):
     return  date.timetuple().tm_yday
 
 def load_clarity_csv(pathname):
-    df = pd.read_csv(pathname, delimiter=";")
-    
+    df = pd.read_csv(pathname)
     #rename
-    columns = ["index","datetime","event_type","event_subtype","patient_info","device_info","source_id","glucose","insulin","carbohydrates","duration","roc","transmitter_time","transmitter_id"]
-    df = df.rename(columns = dict(zip(df.columns.values,columns)))
-    cols_to_remove = set(columns).symmetric_difference(["datetime", "glucose"])
-    df = df.drop(columns=cols_to_remove)
+    columns = ["index",
+               "datetime",
+               "event_type",
+               "event_subtype",
+               "patient_info",
+               "device_info",
+               "source_id",
+               "glucose",
+               "insulin",
+               "carbohydrates",
+               "duration",
+               "roc",
+               "transmitter_time",
+               "transmitter_id"]
+    df = df.rename(columns = dict(zip(df.columns.values, columns)))
+
+    #remove unnecessary columns
+    #cols_to_remove = set(columns).symmetric_difference(["datetime", "glucose"])
+    df = df[["datetime", "glucose"]]
     df = df.dropna(how="any")
+
 
     df["datetime"] = df.datetime.apply(lambda x: datetime.strptime(x, "%Y-%m-%dT%H:%M:%S"))
     #df["timestamp_seconds"] = df.datetime.apply(lambda x: x.timestamp())
